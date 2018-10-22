0f671de66fc7b7ee56ee8afec74268cfafb6f576
tommy-mor <thmorriss@gmail.com>
2018-10-22 13:41:38 -0700

    made quotes work better, added print func

--- src/main/kotlin/entry.kt
+++ src/main/kotlin/entry.kt
@@ -1,5 +1,6 @@
 import com.github.h0tk3y.betterParse.grammar.tryParseToEnd
 import com.github.h0tk3y.betterParse.parser.parseToEnd
+import com.github.h0tk3y.betterParse.parser.toParsedOrThrow
 import com.github.h0tk3y.betterParse.parser.tryParseToEnd
 import java.io.File
 
@@ -7,8 +8,12 @@ fun main(args: Array<String>) {
     var file = File("/Users/tommy/programming/parser/src/main/resources/hudlayout.res")
     //file.forEachLine { println(it) }
     //println(ast.toString())
-    val cleanfile = file.readLines().map { it.replace("\\/\\/.*".toRegex(), "") }
-    println(cleanfile)
+    val cleanfile = file.readLines()
     val a = ItemsParser.tryParseToEnd(cleanfile.joinToString("\n"))
-    println(a)
+    println(ast)
+    println(ast2)
+    val b = a.toParsedOrThrow()
+
+    b.value.forEach { it.print("") }
+
 }
--- src/main/kotlin/parser.kt
+++ src/main/kotlin/parser.kt
@@ -2,12 +2,26 @@ import com.github.h0tk3y.betterParse.combinators.*
 import com.github.h0tk3y.betterParse.grammar.Grammar
 import com.github.h0tk3y.betterParse.grammar.parseToEnd
 import com.github.h0tk3y.betterParse.grammar.parser
+import com.github.h0tk3y.betterParse.grammar.tryParseToEnd
 import com.github.h0tk3y.betterParse.parser.Parser
 
 
-interface Item
-data class Chunk(val title: String, val children: List<Item>) : Item
-data class Entry(val title: String, val value: String) : Item
+interface Item {
+    fun print(indent: String)
+}
+data class Chunk(val title: String, val children: List<Item>) : Item {
+    override fun print(indent: String) {
+        println("$indent\"$title\" {")
+        children.forEach { it.print(indent + "  ") }
+        println("$indent}")
+    }
+}
+data class Entry(val title: String, val value: String) : Item {
+    override fun print(indent: String) {
+        println ("$indent$title    $value")
+    }
+}
+
 
 // todo
 // deal with [XBOX] stuff
@@ -19,16 +33,36 @@ data class Entry(val title: String, val value: String) : Item
 object ItemsParser : Grammar<List<Item>>() {
     val ws by token("\\s+", ignore = true)
 
+    val comment by token("\\/\\/.*", ignore = true)
     val num by token("\\d+")
-    val word by token("""(".+")|[^\s]+""")
+
 
     val LCURL by token("\\{")
     val RCURL by token("}")
 
+    val word by token("""("[^"]+")|[^\s]+""")
+
     val entryParser = word and word map { (a,b) -> Entry(a.text, b.text)}
     val chunkParser = word and -LCURL and zeroOrMore(parser { itemparser }) and -RCURL map { (a,b) -> Chunk(a.text, b) }
-    val itemparser : Parser<Item> = entryParser or chunkParser
+    val itemparser : Parser<Item> = chunkParser or entryParser
     override val rootParser = zeroOrMore(itemparser)
 }
 
-val ast = ItemsParser.parseToEnd("\"a3rtarst\" { starst arsarstars sstst ststst } strstrst { stst stst stst stst sts { stst ststst } }")
+val ast = ItemsParser.parseToEnd("\"a3rtarst\" {  \"zts\" \"xtst\" st  st } stst { } stst stst stst { sts stst } ")
+val ast2 = ItemsParser.parseToEnd("\t\"BasicCrossLarge\"\n" +
+        "\t{\n" +
+        "\t\t\"controlName\"\t\"CExLabel\"\n" +
+        "\t\t\"fieldName\"\t \t\"BasicCrossLarge\"\n" +
+        "\t\t\"visible\"\t\t\"0\"\n" +
+        "\t\t\"enabled\"\t\t\"0\"\n" +
+        "\t\t\"zpos\"\t\t\t\"2\"\n" +
+        "\t\t\"xpos\"\t\t\t\"c-102\"\n" +
+        "\t\t\"ypos\"\t\t\t\"c-98\"\n" +
+        "\t\t\"wide\"\t\t\t\"200\"\n" +
+        "\t\t\"tall\"\t\t\t\"200\"\n" +
+        "\t\t\"font\"\t\t\t\"Crosshairs32\"\n" +
+        "\t\t\"labelText\"\t\t\"2\"\n" +
+        "\t\t\"textAlignment\"\t\"center\"\n" +
+        "\t\t\"fgcolor\"\t\t\"Crosshair\"\n" +
+        "\t}")
+
