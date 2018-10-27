d20b6f6b2380dde2f19ae0b0490114a3be242e83
tommy-mor <thmorriss@gmail.com>
2018-10-27 10:02:35 -0700

    made recursive file eater work

--- src/main/kotlin/entry.kt
+++ src/main/kotlin/entry.kt
@@ -13,12 +13,13 @@ fun main(args: Array<String>) {
     var mainhud = Hud("/Users/tommy/Downloads/rayshud-master")
 }
 
-fun parseFile(file: File) {
+fun parseFile(file: File): List<Item> {
     try {
         val cleanfile = file.readLines()
         val a = ItemsParser.tryParseToEnd(cleanfile.joinToString("\n"))
         println(file.name)
         val b = a.toParsedOrThrow()
+        return b.value
     } catch(e: Exception) {
         println(e.toString())
     }
--- src/main/kotlin/model.kt
+++ src/main/kotlin/model.kt
@@ -2,13 +2,29 @@ import java.io.File
 
 class Hud(filename: String) {
     val rootfile = File(filename)
-    val root = folder(rootfile)
+    val root = folder(rootfile, mutableListOf())
     init {
         if (!rootfile.isDirectory) throw IllegalArgumentException("Hud needs to be given a directory")
+        walk(root)
+
+        println(root)
     }
 
-    fun walk(file: File) : hudfile {
-        file.walkTopDown().maxDepth(1).forEach { if }
+    fun walk(file: folder) : hudfile {
+        file.file.walkTopDown().maxDepth(1).drop(1).forEach {
+            if(it.isDirectory) {
+                var folder = folder(it, mutableListOf())
+                walk(folder)
+                file.children.add(folder)
+            } else {
+                if(it.endsWith(".res")) {
+                    file.children.add(resfile(it))
+                } else {
+                    file.children.add(otherfile(it))
+                }
+            }
+        }
+        return file
     }
     //list of files, list of res files
     //map of filename to chunk objects
@@ -18,6 +34,8 @@ class Hud(filename: String) {
 
 abstract class hudfile
 
-class resfile(val file : File) : hudfile()
+class resfile(val file : File) : hudfile() {
+    var items = parseFile(file)
+}
 class otherfile(val file : File) : hudfile()
-class folder(val file : File, var children: List<hudfile>) : hudfile()
+class folder(val file : File, var children: MutableList<hudfile>) : hudfile()
